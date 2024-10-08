457bea501816a4cb9006361e5053c9a0bfaadfdc
guilherme-paiva1 <guilherme.paiva@sptech.school>
2024-10-07 21:43:29 -0300

    adicionando machine de ascii

--- script.js
+++ script.js
@@ -45,12 +45,14 @@ function mostrarDecimal() {
     div_binario.style.opacity = 0;
     div_octal.style.opacity = 0;
     div_hexa.style.opacity = 0;
+    div_ascii.style.opacity = 0;
 
     setTimeout(function() {
         div_binario.style.display = 'none';
         div_octal.style.display = 'none';
         div_hexa.style.display = 'none';
-    }, 600);
+        div_ascii.style.display = 'none';
+        }, 600);
 
     if (div_decimal.style.opacity == 0) {
         mostrarDiv(div_decimal); 
@@ -61,11 +63,13 @@ function mostrarBinario() {
     div_decimal.style.opacity = 0;
     div_octal.style.opacity = 0;
     div_hexa.style.opacity = 0;
+    div_ascii.style.opacity = 0;
 
     setTimeout(function() {
         div_decimal.style.display = 'none';
         div_octal.style.display = 'none';
         div_hexa.style.display = 'none';
+        div_ascii.style.display = 'none';
     }, 600);
 
     if (div_binario.style.opacity == 0) {
@@ -78,11 +82,13 @@ function mostrarOctal() {
     div_binario.style.opacity = 0;
     div_decimal.style.opacity = 0;
     div_hexa.style.opacity = 0;
+    div_ascii.style.opacity = 0;
 
     setTimeout(function() {
         div_binario.style.display = 'none';
         div_decimal.style.display = 'none';
         div_hexa.style.display = 'none';
+        div_ascii.style.display = 'none';
     }, 600);
     
     if (div_octal.style.opacity == 0) {
@@ -94,15 +100,35 @@ function mostrarHexadecimal() {
     div_binario.style.opacity = 0;
     div_octal.style.opacity = 0;
     div_decimal.style.opacity = 0;
+    div_ascii.style.opacity = 0;
 
     setTimeout(function() {
         div_binario.style.display = 'none';
         div_octal.style.display = 'none';
         div_decimal.style.display = 'none';
+        div_ascii.style.display = 'none';
     }, 600);
 
     if (div_hexa.style.opacity == 0) {
         mostrarDiv(div_hexa);
+    }
+}
+
+function mostrarASCII() {
+    div_binario.style.opacity = 0;
+    div_octal.style.opacity = 0;
+    div_decimal.style.opacity = 0;
+    div_hexa.style.opacity = 0;
+
+    setTimeout(function() {
+        div_binario.style.display = 'none';
+        div_octal.style.display = 'none';
+        div_decimal.style.display = 'none';
+        div_hexa.style.display = 'none';
+    }, 600);
+
+    if (div_hexa.style.opacity == 0) {
+        mostrarDiv(div_ascii);
     }
 }
 
@@ -239,4 +265,92 @@ function converterHexa() {
     calcularHexOctal();
     calcularHexDec();
     calcularHexBin();
+}
+
+// Funções para ASCII
+function calcularAsciiDec() {
+    var caractere = input_ascii.value;
+    var soma = 0;
+    var bin = '';
+    var binDec = 0;
+    var letra = caractere.split('');
+    
+    letra.forEach(element => {
+        element = element.charCodeAt();
+        bin = '0' + element.toString(2);
+        binDec = parseInt(bin, 2);
+        soma += binDec;
+    });
+
+    document.getElementById("div_asciiDec").innerHTML = soma;
+}
+
+function calcularAsciiBin() {
+    var caractere = input_ascii.value;
+    var mensagem = '';
+    var bin = '';
+    var letra = caractere.split('');
+    
+
+    letra.forEach(element => {
+        element = element.charCodeAt();
+        bin = '0' + element.toString(2);
+
+        mensagem += `${bin} `;
+    });
+
+    document.getElementById("div_asciiBin").innerHTML = mensagem;
+}
+
+function calcularAsciiOctal() {
+    var caractere = input_ascii.value;
+    var mensagem = '';
+    var bin = '';
+    var octal = '';
+    var binDec = 0;
+    var letra = caractere.split('');
+
+    letra.forEach(element => {
+        element = element.charCodeAt();
+        bin = '0' + element.toString(2);
+        binDec = parseInt(bin, 2);
+        octal = binDec.toString(8);
+
+        mensagem += `${octal} `;
+    });
+
+    document.getElementById("div_asciiOctal").innerHTML = mensagem;
+}
+
+function calcularAsciiHexa() {
+    var caractere = input_ascii.value;
+    var mensagem = '';
+    var bin = '';
+    var hexa = '';
+    var binDec = 0;
+    var letra = caractere.split('');
+    
+
+    letra.forEach(element => {
+        element = element.charCodeAt();
+        bin = '0' + element.toString(2);
+        binDec = parseInt(bin, 2);
+        hexa = binDec.toString(16);
+
+        mensagem += `${hexa} `;
+    });
+
+    document.getElementById("div_asciiHexa").innerHTML = mensagem;
+}
+
+function converterCaracter() {
+    mostrarDivResultado(div_asciiDec);
+    mostrarDivResultado(div_asciiBin);
+    mostrarDivResultado(div_asciiOctal);
+    mostrarDivResultado(div_asciiHexa);
+
+    calcularAsciiBin();
+    calcularAsciiDec();
+    calcularAsciiHexa();
+    calcularAsciiOctal();
 }
